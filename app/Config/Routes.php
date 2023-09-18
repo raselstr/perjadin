b0ab02da042120ece03f0e7e34f5b00b60b16591
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2023-09-18 19:12:41 +0700

    membuat login dan register

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/login','Home::login');
+
+$routes->presenter('auth');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,9 @@ class Home extends BaseController
     {
         return view('layout/dashboard');
     }
+
+    public function login()
+    {
+        return view('layout/login');
+    }
 }
