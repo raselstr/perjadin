1f2ac3c4bdb6a05567df1db7f2acbc6f8d59951f
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-02-02 22:52:52 +0700

    menonaktifkan toolbar

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -4,10 +4,10 @@ namespace Config;
 
 use App\Filters\AuthFilter;
 use App\Filters\RoleFilter;
-use CodeIgniter\Filters\CSRF;
-use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
+use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
 use CodeIgniter\Filters\SecureHeaders;
 
@@ -21,13 +21,13 @@ class Filters extends BaseConfig
      * @phpstan-var array<string, class-string>
      */
     public array $aliases = [
-        'csrf'          => CSRF::class,
-        'toolbar'       => DebugToolbar::class,
-        'honeypot'      => Honeypot::class,
-        'invalidchars'  => InvalidChars::class,
+        'csrf' => CSRF::class,
+        'toolbar' => DebugToolbar::class,
+        'honeypot' => Honeypot::class,
+        'invalidchars' => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
-        'authfilter'    => AuthFilter::class,
-        'rolefilter'    => RoleFilter::class,
+        'authfilter' => AuthFilter::class,
+        'rolefilter' => RoleFilter::class,
     ];
 
     /**
@@ -40,13 +40,13 @@ class Filters extends BaseConfig
     public array $globals = [
         'before' => [
             'authfilter' => ['except' => ['login', 'loginProses', 'logout']],
-            'rolefilter'=> ['except' => ['login','loginProses','error','logout']],
+            'rolefilter' => ['except' => ['login', 'loginProses', 'error', 'logout']],
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
         ],
         'after' => [
-            'toolbar',
+            // 'toolbar',
             // 'honeypot',
             // 'secureheaders',
         ],
