990c2afa2c4743693ca98b1b49987e4acbabd502
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-02-15 18:59:40 +0700

    versi aplikasi

--- app/Views/layout/default.php
+++ app/Views/layout/default.php
@@ -39,9 +39,9 @@
   <?= $this->renderSection('modalcustome'); ?>
   <footer class="main-footer">
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0
+      <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; <code>Raselstr</code> - Template By : AdminLTE.io</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
