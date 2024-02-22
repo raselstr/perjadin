6b290829740f0bb2acfeb09dc2edcb66ded24654
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-02-23 00:57:41 +0700

    belum siap spd back

--- app/Views/pelaksana/spdback.php
+++ app/Views/pelaksana/spdback.php
@@ -7,24 +7,155 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <table class="table table-striped table-bordered">
-  <thead>
-    ...
-  </thead>
-  <tbody>
-    ...
-    <tr>
-      <td colspan="4">
-        <table class="table mb-0">
-          ...
-        </table>
-      </td>
-    </tr>
-    ...
-  </tbody>
-</table>
-    </div>
-    
+      <table class="table table-bordered">
+        <tr>
+          <td>
+            <table class="tableb mb-0 table-bordered">
+              <tr>
+                
+              </tr>
+              <tr>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class="table mb-0 table-sm table-bordered">
+              <tr>
+                <td>I.</td>
+                <td>Berangkat dari</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(Tempat Kedudukan)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Ke</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table class="table mb-0 table-sm table-bordered">
+              <tr>
+                <td>I.</td>
+                <td>Berangkat dari</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(Tempat Kedudukan)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Ke</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class="table mb-0 table-sm table-bordered">
+              <tr>
+                <td>I.</td>
+                <td>Berangkat dari</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(Tempat Kedudukan)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Ke</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+          </td>
+        </tr>
+      </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
