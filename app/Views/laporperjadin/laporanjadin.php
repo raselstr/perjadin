9532997d79601abb90a1d83fabbb21de9f910d9b
Rasel S Str <54805845+raselstr@users.noreply.github.com>
2024-02-26 12:22:49 +0700

    Laporan Cetak Jadin

---
+++ app/Views/laporperjadin/laporanjadin.php
@@ -0,0 +1,406 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= $title; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <h5>
+      <table class="table table-bordered border-black">
+        <tr>
+          <td>
+            <table class="tableb mb-0 table-bordered">
+              <tr >
+              </tr>
+            </table>
+          </td>
+          <td width="50%">
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%">I.</td>
+                <td width="35%">Berangkat dari</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(Tempat Kedudukan)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Ke</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%">II.</td>
+                <td width="35%">Tiba di</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%"></td>
+                <td width="35%">Berangkat dari</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Ke</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%">III.</td>
+                <td width="35%">Tiba di</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%"></td>
+                <td width="35%">Berangkat dari</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Ke</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%">IV.</td>
+                <td width="35%">Tiba di</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%"></td>
+                <td width="35%">Berangkat dari</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Ke</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td width="10%">V.</td>
+                <td width="35%">Tiba di</td>
+                <td width="5%">:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(Tempat Kedudukan)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>Pada Tanggal</td>
+                <td>:</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">Kepala</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">&nbsp;</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td></td>
+                <td colspan="3">NIP.</td>
+              </tr>
+            </table>
+          </td>
+          <td>
+            <table class="table mb-0 table-sm table-borderless">
+              <tr>
+                <td colspan="4">Telah diperiksa dengan keterangan
+                  bahwa perjalanan tersebut atas
+                  perintahnya dan semata-mata
+                  untuk kepentingan jabatan dalam
+                  waktu yang sesingkat-singkatnya
+                </td>
+              </tr>
+              <tr>
+                <td colspan="4">Kepala</td>
+              </tr>
+              <tr>
+                <td colspan="4">&nbsp;</td>
+              </tr>
+              <tr>
+                <td colspan="4">&nbsp;</td>
+              </tr>
+              <tr>
+                <td colspan="4">(.........................................................................)</td>
+              </tr>
+              <tr>
+                <td colspan="4">NIP.</td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">Catatan Lain-lain</td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            VI. Perhatian :<br>
+            Pejabat yang menerbitkan SPD, Pegawai yang melakukan perjalanan dinas,
+            para pejabat yang mengesahkan tanggal berangkat/tiba, serta bendahara
+            pengeluaran bertanggung jawab berdasarkan peraturan keuangan, apabila
+            daerah menderita kerugian akibat kelalaian, kesalahan, dan kealpaannya.
+          </td>
+        </tr>
+      </table>
+    </h5>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+      window.addEventListener("load", window.print());
+    </script>
+  </body>
+</html>
